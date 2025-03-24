ccc96bda852784d77507218a00bfcf168ba33ba2
90545 <90545@YASINEFEDEMIR>
2025-03-24 06:21:08 +0300

    Veritabanı oluşturuldu bağlantı yapıldı

---
+++ Presentaton/CarGo.WebApi/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+using CarGo.Persistence.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// PostgreSQL baðlantýsýný ekleyin (appsettings.json'dan alarak)
+builder.Services.AddDbContext<CarGoContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
